4689b8b7a3c83aba960b62e296768cc32d643d88
Thunraz <thunraz@thunraz.com>
2018-07-25 14:53:05 +0200

    Added recording functionality

--- examples/sandbox/sandbox.js
+++ examples/sandbox/sandbox.js
@@ -32,16 +32,77 @@ class Square extends Minim2D.Object2D {
     }
 }
 
+class Recorder {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.isRecording = false;
+    }
+
+    toggleRecording() {
+        this.isRecording = !this.isRecording;
+
+        if(this.isRecording) {
+            /* eslint-disable-next-line */
+            console.log('Starting to record');
+            this.stream = this.canvas.captureStream();
+            this.recordedBlobs = [];
+
+            let options = { mimeType: 'video/webm', videoBitsPerSecond: 4000e3 };
+            try {
+                this.mediaRecorder = new MediaRecorder(this.stream, options);
+
+                this.mediaRecorder.ondataavailable = (event) => {
+                    if (event.data && event.data.size > 0) {
+                        this.recordedBlobs.push(event.data);
+                    }
+                };
+                this.mediaRecorder.start(100); // collect 100ms of data
+            } catch(e) {
+                console.warn('Unable to create MediaRecorder with options Object: ', e);
+            }
+        } else {
+            /* eslint-disable-next-line */
+            console.log('Stopping to record');
+            this.mediaRecorder.stop();
+            let blob = new Blob(this.recordedBlobs, { type: 'video/webm' });
+            let url  = window.URL.createObjectURL(blob);
+            let a    = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'output.webm';
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 100);
+        }
+    }
+}
+
+const BUTTON_TIMEOUT = 0.25;
+let recorder;
+
 function handleControls(states) {
+    if(states.record && states.buttonTimeout <= 0.0 && recorder) {
+        states.resetButtonTimeout(BUTTON_TIMEOUT);
+        recorder.toggleRecording();
+    }
 }
 
 
 (function() {
+    let customKeyCodes = {
+        80: 'record'
+    };
+
     let camera = new Minim2D.Camera();
-    let game   = new Minim2D.Game(camera, document.getElementById('g'));
+    let game   = new Minim2D.Game(camera, document.getElementById('g'), { customKeyCodes, buttonTimeout: BUTTON_TIMEOUT });
     let scene  = new Minim2D.Scene();
     game.setScene(scene);
     game.gameLoop();
+
+    recorder = new Recorder(game.canvas);
 
     window.addEventListener('handleControls', (e) => handleControls(e.detail), false);
 

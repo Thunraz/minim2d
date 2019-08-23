3df18790782975592dc8e00204808ed035022dc3
Thunraz <thunraz@thunraz.com>
2019-08-23 08:04:32 +0200

    Fiddle around in sandbox

--- examples/sandbox/sandbox.js
+++ examples/sandbox/sandbox.js
@@ -9,8 +9,8 @@ class Square extends Minim2D.Object2D {
         this.add(this.bitmap);
         this.timer = 0.0;
         
-        this.position.x = 150 +  Math.cos(this.timer) * 100;
-        this.position.y = 150 +  Math.sin(this.timer) * 100;
+        // this.position.x = 150 +  Math.cos(this.timer) * 100;
+        // this.position.y = 150 +  Math.sin(this.timer) * 100;
 
         this.orbitSpeed = orbitSpeed;
     }
@@ -20,10 +20,10 @@ class Square extends Minim2D.Object2D {
 
         this.timer += dt;
 
-        this.position.x = 150 +  Math.cos(this.timer * this.orbitSpeed) * 100;
-        this.position.y = 150 +  Math.sin(this.timer * this.orbitSpeed) * 100;
+        // this.position.x = 150 +  Math.cos(this.timer * this.orbitSpeed) * 100;
+        // this.position.y = 150 +  Math.sin(this.timer * this.orbitSpeed) * 100;
 
-        this.rotation = Math.sin(this.timer * dt * 100) * 10;
+        this.rotation = Math.sin(this.timer * dt * 10) * 10;
     }
 
     handleControls(states, camera, game, dt) {
@@ -37,8 +37,6 @@ class Square extends Minim2D.Object2D {
 
 const BUTTON_TIMEOUT = 0.25;
 const CAMERA_PAN_AMT = 2;
-const square1 = new Square('./minim2d_a.png', 1.0);
-square1.bitmap.origin = new Minim2D.Vector2(16, 32);
 let recorder;
 
 function handleControls(states, camera, game, dt) {
@@ -64,7 +62,7 @@ function handleControls(states, camera, game, dt) {
         game.soundManager.play('blip');
     }
 
-    square1.handleControls(states, camera, game, dt);
+    // square1.handleControls(states, camera, game, dt);
 }
 
 
@@ -90,18 +88,22 @@ function handleControls(states, camera, game, dt) {
     recorder = new Minim2D.Recorder(game.canvas);
 
     window.addEventListener('handleControls', (e) => handleControls(e.detail.states, camera, game, e.detail.deltaT), false);
-
+    
+    const square1 = new Square('./minim2d_a.png', 1.0);
+    square1.position.x = 150;
+    square1.position.y = 150;
     scene.add(square1);
 
-    const square2 = new Square('./minim2d_a.png', 2.0);
-    scene.add(square2);
+    const square2 = new Square('./minim2d_b.png', 2.0);
+    square2.position.x = 40;
+    square1.add(square2);
+    // scene.add(square2);
 
-    const square3 = new Square('./minim2d_a.png', 3.0);
-    square3.zIndex = -1;
-    square3.bitmap.position.x = 20;
-    scene.add(square3);
+    const square3 = new Square('./minim2d_c.png', 3.0);
+    square3.position.x = 40;
+    square2.add(square3);
 
-    const square4 = new Square('./minim2d_b.png', 0);
-    square4.renderFixed = true;
-    scene.add(square4);
+    const square4 = new Square('./minim2d_a.png', 3.0);
+    square4.position.x = 60;
+    square2.add(square4);
 }());

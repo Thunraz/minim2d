2667f956b40570d8f8475e02f1c3e095bbba6afd
Thunraz <thunraz@thunraz.com>
2021-01-01 19:55:55 +0100

    Linter/OCD fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/order
 import * as pkg from './package.json';
 
 import babel       from 'rollup-plugin-babel';
@@ -5,29 +6,29 @@ import { eslint  } from 'rollup-plugin-eslint';
 import replace     from 'rollup-plugin-replace';
 
 export default {
-    input: 'src/Minim2D.js',
+    input:   'src/Minim2D.js',
     plugins: [
         eslint(),
         replace({
-            VERSION: pkg.version
+            VERSION: pkg.version,
         }),
         babel({
-            exclude: 'node_modules/**' // only transpile our source code
-        })
+            exclude: 'node_modules/**', // only transpile our source code
+        }),
     ],
     output: [
         {
-            format: 'umd',
-            name: 'Minim2D',
-            file: 'build/minim2d.js',
-            indent: '    ',
-            sourcemap: false
+            format:    'umd',
+            name:      'Minim2D',
+            file:      'build/minim2d.js',
+            indent:    '    ',
+            sourcemap: false,
         },
         {
-            format: 'es',
-            file: 'build/minim2d.module.js',
-            indent: '    ',
-            sourcemap: false
-        }
-    ]
+            format:    'es',
+            file:      'build/minim2d.module.js',
+            indent:    '    ',
+            sourcemap: false,
+        },
+    ],
 };
